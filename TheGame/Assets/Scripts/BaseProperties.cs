5809600e8ae6e7c6096d8ffabbb3eb4747a00c21
Erik Broberg <erikbroberg90@gmail.com>
2015-03-01 17:40:13 -0800

    simple Base prefab, dynamically scales and changes color wrt numUnits

---
+++ TheGame/Assets/Scripts/BaseProperties.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseProperties : MonoBehaviour {
+
+	// All public for easy debugging in Unity
+	public int numUnits = 5;
+	public int unitCapacity = 20;
+	public float unitGenerationRate = 2;
+
+
+	public GameObject icon;
+
+	// Use this for initialization
+	void Start () {
+		icon = transform.Find ("base_icon").gameObject;
+
+		//Update number of units in Base every "unitGenerationRate" second
+		InvokeRepeating ("UpdateUnits", -1, 1.0f/unitGenerationRate);
+	}
+	
+	
+	void Update () {
+		float newSize = Mathf.Sqrt(1 + numUnits/5.0f);
+		icon.transform.localScale = new Vector3(newSize, newSize, 1);
+	}
+
+	// Increase or decrease units wrt base capacity.
+	void UpdateUnits(){
+		if (numUnits < unitCapacity) {
+			numUnits++;
+			icon.GetComponent<IconProperties>().setColor(Color.blue);
+		}
+		else if (numUnits > unitCapacity){
+			numUnits--;
+			icon.GetComponent<IconProperties>().setColor(Color.red);
+		}
+	}
+}
---
+++ TheGame/Assets/Scripts/IconProperties.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class IconProperties : MonoBehaviour {
+
+	public SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponents<SpriteRenderer> ()[0];
+		if (spriteRenderer == null) {
+			throw new System.Exception("Couldn't find SpriteRenderer!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setColor(Color c){
+		spriteRenderer.color = c;
+	}
+}
